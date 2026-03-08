66b7fb2fba3e52430a425406b99fe59e368c7898
Gianluca Fucci <100246766+Gianluca-X@users.noreply.github.com>
2026-03-08 15:05:11 +0000

    fix user pars4e

--- src/utils/parseData/index.ts
+++ src/utils/parseData/index.ts
@@ -8,18 +8,19 @@ export const parseRecordContent = (record: any, variant: any): IRecord => {
 };
 
 export function parseJwt(token: string) {
-  if (!token) {
-    return null;
-  }
-  const base64Url = token.split('.')[1];
+  if (!token) return null;
+
+  const parts = token.split('.');
+  if (parts.length < 2) return null;
+
+  const base64Url = parts[1];
   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+
   const jsonPayload = decodeURIComponent(
     window
       .atob(base64)
       .split('')
-      .map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      })
+      .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
       .join('')
   );
 
