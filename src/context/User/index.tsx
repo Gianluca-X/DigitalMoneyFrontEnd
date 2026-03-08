46c09fcaedd29030bb81d93f35df4510f3a42ef3
Gianluca Fucci <100246766+Gianluca-X@users.noreply.github.com>
2026-03-08 18:36:13 +0000

    fix

--- src/context/User/index.tsx
+++ src/context/User/index.tsx
@@ -60,7 +60,7 @@ const UserInfoProvider = ({ children }: { children: React.ReactNode }) => {
       console.log(error);
     });
 
-}, [token]);
+}, [token, setIsAuthenticated, setToken]);
 
   return (
     <userInfoContext.Provider
